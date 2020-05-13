248ba2f23ff360de4b0c25b168c701df69d513c8
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-13 17:29:25 +0100

    serialize search recipes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,33 @@ class SearchController < ApplicationController
 
 	def search
 		recipes = Recipe.search(params[:query])
-		render json: recipes, status: :accepted
+		if recipes
+			render json: recipes.to_json(
+				only: [:title, :description, :imageLink, :prepTime, :cookingTime, :instructions, :servingCount, :uuid],
+				include: {
+					user: {
+						only: [:username]
+					},
+					recipe_ingredients: {
+						only: [:weight],
+						include: {
+							ingredient: {
+								only: [:uuid, :name]
+							}
+						}
+					},
+					recipe_stars: {
+						only: [],
+						include: {
+							user: {
+								only: [:username]
+							}
+						}
+					}
+				}
+			), status: :accepted
+		else
+			render json: { error: 'failed to find recipe' }, status: :not_acceptable
+		end
 	end
 end
