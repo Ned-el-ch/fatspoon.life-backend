8d331abf70654d68412e773a756662f0018a6592
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-13 16:58:27 +0100

    add pg_search_scope to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,14 @@
 class Recipe < ApplicationRecord
+
+	include PgSearch::Model
+	pg_search_scope :search_by_term, against: [:title, :description, :instructions],
+		using: {
+			tsearch: {
+				any_word: true,
+				prefix: true
+			}
+		}
+	
 	belongs_to :user
 	has_many :recipe_stars
 	has_many :users, through: :recipe_stars
