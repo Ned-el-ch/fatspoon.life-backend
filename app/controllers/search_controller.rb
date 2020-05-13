d0c8c4ffb56cdfb87934cc6cd7c8f546e5c40b50
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-13 17:09:49 +0100

    Update search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
 	skip_before_action :authorized, only: [:search]
 
 	def search
-		@recipes = Recipe.search_by_term(params[:query])
+		@recipes = Recipe.multisearch(params[:query])
 		render json: @recipes
 	end
 end
