befb899d507c9875c64e5f406a6e962bc13b7679
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-13 17:03:17 +0100

    add multisearchable migration

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,14 @@
 class Recipe < ApplicationRecord
 
-	include PgSearch::Model
-	pg_search_scope :search_by_term, against: [:title, :description, :instructions],
-		using: {
-			tsearch: {
-				any_word: true,
-				prefix: true
-			}
-		}
+	include PgSearch
+	multisearchable :against => [:title, :description, :instructions]
+	# pg_search_scope :search_by_term, against: [:title, :description, :instructions],
+	# 	using: {
+	# 		tsearch: {
+	# 			any_word: true,
+	# 			prefix: true
+	# 		}
+	# 	}
 	
 	belongs_to :user
 	has_many :recipe_stars
