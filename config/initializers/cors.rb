26308e54efb30f9988ff0dbb64fd8e3ccdea675c
Ned-el-ch <nedelchev.niki@gmail.com>
2020-05-14 07:52:04 +0100

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fatspoon.life'
+    origins '*'
+    # origins 'https://fatspoon.life'
 
     resource '*',
       headers: :any,
